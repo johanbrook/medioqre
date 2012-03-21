f6c055e3d263a78d9b9c58bb0e1defb10e575c69
Johan Brook <johan.jvb@gmail.com>
2012-03-21 16:12:34 +0100

    Added GameModel

---
+++ src/model/GameModel.java
@@ -0,0 +1,20 @@
+package model;
+
+import constants.Direction;
+
+/**
+ * Model for a game.
+ * 
+ * @author Johan
+ *
+ */
+public class GameModel {
+	
+	public GameModel() {
+		
+	}
+	
+	public void updateDirection(Direction dir) {
+		
+	}
+}
