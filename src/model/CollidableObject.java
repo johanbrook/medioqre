5bed699a3aa5e33cf68087b50e1998e4e9bd752f
Johan Brook <johan.jvb@gmail.com>
2012-03-21 14:55:31 +0100

    Added some code documentation

--- src/model/CollidableObject.java
+++ src/model/CollidableObject.java
@@ -1,11 +1,11 @@
-/**
-*	CollidableObject.java
-*
-*	@author Johan
-*/
-
 package model;
 
+/**
+ * The collidable object super class
+ * 
+ * @author Johan
+ *
+ */
 public abstract class CollidableObject {
 	
 	private java.awt.Dimension collisionBox;
@@ -24,18 +24,40 @@ public abstract class CollidableObject {
 	}
 	
 	
+	/**
+	 * Get the position of the object in the game world.
+	 * 
+	 * @return The position
+	 */
 	public Position getPosition(){
 		return this.position;
 	}
 	
+	/**
+	 * Set the position of the object in the game world.
+	 * 
+	 * @param pos The position
+	 */
 	public void setPosition(Position pos){
 		this.position = pos;
 	}
 	
+	/**
+	 * Set the position of the object in the game world.
+	 * 
+	 * @param x The X coordinate
+	 * @param y The Y coordinate
+	 */
 	public void setPosition(double x, double y) {
 		this.position = new Position(x, y);
 	}
 	
+	
+	/**
+	 * Get the collision box of the object.
+	 * 
+	 * @return The collision box
+	 */
 	public java.awt.Dimension getCollisionBox(){
 		return this.collisionBox;
 	}
--- src/model/weapon/WeaponBelt.java
+++ src/model/weapon/WeaponBelt.java
@@ -1,9 +1,3 @@
-/**
-*	WeaponBelt.java
-*
-*	@author Johan
-*/
-
 package model.weapon;
 
 import java.util.Arrays;
@@ -11,27 +5,59 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * The weapon belt class
+ * 
+ * @author Johan
+ *
+ */
 public class WeaponBelt {
 	private List<IWeapon> weapons;
 	
+	/**
+	 * Create a new weapon belt with the four standard weapons (machine gun, grenade,
+	 * sword and portal gun).
+	 */
 	public WeaponBelt() {
 		IWeapon[] temp = {new MachineGun(), new Sword(), new Grenade(), new PortalGun()};
 		
 		this.weapons = new LinkedList<IWeapon>(Arrays.asList(temp));
 	}
 	
+	/**
+	 * Create a new weapon belt from a collection of weapons.
+	 * 
+	 * @param coll The collection.
+	 */
 	public WeaponBelt(Collection<? extends IWeapon> coll){
 		this.weapons = new LinkedList<IWeapon>(coll);
 	}
 	
+	/**
+	 * Add a weapon to the belt.
+	 * 
+	 * @param w The weapon
+	 */
 	public void addWeapon(IWeapon w) {
 		this.weapons.add(w);
 	}
 	
+	/**
+	 * Get the weapon in the belt from a certain slot.
+	 * 
+	 * @param slot The slot
+	 * @return The weapon on that slot
+	 */
 	public IWeapon getWeapon(int slot) {
 		return this.weapons.get(slot);
 	}
 	
+	/**
+	 * Get the weapon of a certain type from the belt.
+	 * 
+	 * @param type A weapon type (the weapon's class)
+	 * @return The first weapon in the belt with the specified type
+	 */
 	public IWeapon getWeapon(Class<? extends IWeapon> type) {
 		
 		for(IWeapon w : this.weapons) {
