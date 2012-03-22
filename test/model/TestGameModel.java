98d2668b3667c76770c94a122bdb6b13c278f652
Johan Brook <johan.jvb@gmail.com>
2012-03-22 12:45:55 +0100

    Added real tests for GameModel

--- test/model/TestGameModel.java
+++ test/model/TestGameModel.java
@@ -10,31 +10,51 @@ import static org.junit.Assert.*;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import constants.Direction;
+
 public class TestGameModel implements PropertyChangeListener {
 
-	private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 	private GameModel game;
+	private boolean notified;
+	private Direction oldDir;
+	private Direction newDir;
 	
 	@Before
 	public void setUp() throws Exception {
+		this.notified = false;
 		this.game = new GameModel();
 		this.game.addObserver(this);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFirePropertyChange() {
+		this.game.updateDirection(Direction.NORTH);
+		assertTrue(this.notified);
 	}
+	
+	@Test
+	public void testUpdateDirection() {
+		this.game.updateDirection(Direction.NORTH);
+		
+		assertEquals(Direction.NORTH, this.newDir);
+	}
+	
 
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
-		// TODO Auto-generated method stub
-		
+		this.notified = true;
+		this.oldDir = (Direction) evt.getOldValue();
+		this.newDir = (Direction) evt.getNewValue();
+	}
+	
+	@After
+	public void cleanUp() {
+		this.game.removeObserver(this);
 	}
 
 }
