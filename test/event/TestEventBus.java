d678be7e4aefc39e441c9344ca09d0378a5956f4
Johan Brook <johan.jvb@gmail.com>
2012-03-26 17:50:55 +0200

    Added initial test for EventBus

---
+++ test/event/TestEventBus.java
@@ -0,0 +1,72 @@
+/**
+*	TestEventBus.java
+*
+*	@author Johan
+*/
+
+package event;
+
+import static org.junit.Assert.*;
+
+import model.character.Player;
+import model.weapon.Sword;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import event.Event.Property;
+
+public class TestEventBus {
+
+	private Subscriber handler;
+	private Player publisher;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.handler = new Subscriber();
+		this.publisher = new Player();
+		
+		EventBus.INSTANCE.register(this.handler);
+		
+		this.publisher.setCurrentWeapon(Sword.class);
+	}
+
+	@Test
+	public void testDidReceiveEvent() {
+		
+		assertTrue(this.handler.didReceiveEvent());
+	}
+	
+	@Test
+	public void testProperty(){
+		
+		assertEquals(Property.CHANGED_WEAPON, this.handler.getEvent().getProperty());
+	}
+	
+	@Test
+	public void testSource() {
+		Player p = (Player) this.handler.getEvent().getValue();
+		assertEquals(this.publisher, p);
+	}
+	
+	private class Subscriber implements IEventHandler {
+		private boolean didReceiveEvent = false;
+		private Event event;
+		
+		@Override
+		public void onEvent(Event evt) {
+			this.didReceiveEvent = true;
+			this.event = evt;
+		}
+		
+		public boolean didReceiveEvent() {
+			return this.didReceiveEvent;
+		}
+		
+		public Event getEvent() {
+			return this.event;
+		}
+		
+	}
+
+}
