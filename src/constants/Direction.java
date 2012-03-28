50d630ed52a462507ad6339eac013b0d844463eb
Johan Brook <johan.jvb@gmail.com>
2012-03-28 16:25:35 +0200

    Fixed incorrect diagonal movement

--- src/constants/Direction.java
+++ src/constants/Direction.java
@@ -8,8 +8,10 @@ package constants;
  */
 public enum Direction {
 	NORTH(0, -1), EAST(1,0), SOUTH(0,1), WEST(-1, 0),
-	NORTH_EAST(0.7071, 0.7071), SOUTH_EAST(0.7071, -0.7071),
-	NORTH_WEST(-0.7071, 0.7071), SOUTH_WEST(-0.7071, -0.7071),
+	
+	NORTH_EAST(0.7071, -0.7071), SOUTH_EAST(0.7071, 0.7071),
+	NORTH_WEST(-0.7071, -0.7071), SOUTH_WEST(-0.7071, 0.7071),
+	
 	ORIGIN(0,0);
 	
 	private final double xratio;
