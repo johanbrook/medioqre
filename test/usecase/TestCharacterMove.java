d0c095e09ab8fadb6e6f1b76d0b0fa4abc7eceda
Johan Brook <johan.jvb@gmail.com>
2012-03-28 22:52:59 +0200

    Added tests for diagonal movement

--- test/usecase/TestCharacterMove.java
+++ test/usecase/TestCharacterMove.java
@@ -8,8 +8,6 @@ package usecase;
 
 import static org.junit.Assert.*;
 
-import java.awt.Dimension;
-import java.awt.Rectangle;
 
 import model.character.*;
 import model.character.Character;
@@ -18,17 +16,22 @@ import model.Position;
 import org.junit.Before;
 import org.junit.Test;
 
+import constants.Direction;
+
 
 public class TestCharacterMove {
 
 	private Character player;
-	private Character enemy;
 	private final double DELTA = 1.0;
+	
+	// How far a player moves in 1.0 dt.
+	private int playerMovement;
 	
 	@Before
 	public void setUp() throws Exception {
 		this.player = new Player();
-		this.enemy = new Enemy(7, 30, new Rectangle(), new Dimension(), 0, 0);
+		int playerSpeed = this.player.getMovementSpeed();
+		this.playerMovement = (int) ((int) playerSpeed * DELTA);
 	}
 
 	
@@ -41,12 +44,9 @@ public class TestCharacterMove {
 	@Test
 	public void testMoveNorth() {
 		this.player.setDirection(constants.Direction.NORTH);
-		this.enemy.setDirection(constants.Direction.NORTH);
 		this.player.move(DELTA);
-		this.enemy.move(DELTA);
 		
-		assertEquals(new Position(0, (int)-DELTA*10), this.player.getPosition());
-		assertEquals(new Position(0, (int)-DELTA*7), this.enemy.getPosition());
+		assertEquals(new Position(0, -this.playerMovement), this.player.getPosition());
 	}
 	
 	
@@ -55,6 +55,65 @@ public class TestCharacterMove {
 		this.player.setDirection(constants.Direction.SOUTH);
 		this.player.move(DELTA);
 		
-		assertEquals(new Position(0,10), this.player.getPosition());
+		assertEquals(new Position(0, this.playerMovement), this.player.getPosition());
+	}
+	
+	@Test
+	public void testMoveEast() {
+		this.player.setDirection(constants.Direction.EAST);
+		this.player.move(DELTA);
+		
+		assertEquals(new Position(this.playerMovement,0), this.player.getPosition());
+	}
+	
+	@Test
+	public void testMoveWest() {
+		this.player.setDirection(constants.Direction.WEST);
+		this.player.move(DELTA);
+		
+		assertEquals(new Position(-this.playerMovement,0), this.player.getPosition());
+	}
+	
+	@Test
+	public void testMoveNorthWest() {
+		this.player.setDirection(constants.Direction.NORTH_WEST);
+		this.player.move(DELTA);
+		int xmovement = (int) (Direction.NORTH_WEST.getXRatio() * this.playerMovement);
+		int ymovement = (int) (Direction.NORTH_WEST.getYRatio() * this.playerMovement);
+		
+		assertEquals(new Position(xmovement, ymovement), this.player.getPosition());
+	}
+	
+	@Test
+	public void testMoveNorthEast() {
+		this.player.setDirection(constants.Direction.NORTH_EAST);
+		this.player.move(DELTA);
+		
+		int xmovement = (int) (Direction.NORTH_EAST.getXRatio() * this.playerMovement);
+		int ymovement = (int) (Direction.NORTH_EAST.getYRatio() * this.playerMovement);
+		
+		assertEquals(new Position(xmovement, ymovement), this.player.getPosition());
+	}
+	
+	@Test
+	public void testMoveSouthWest() {
+		this.player.setDirection(constants.Direction.SOUTH_WEST);
+		this.player.move(DELTA);
+		
+		int xmovement = (int) (Direction.SOUTH_WEST.getXRatio() * this.playerMovement);
+		int ymovement = (int) (Direction.SOUTH_WEST.getYRatio() * this.playerMovement);
+		
+		assertEquals(new Position(xmovement, ymovement), this.player.getPosition());
+	}
+	
+	@Test
+	public void testMoveSouthEast() {
+		this.player.setDirection(constants.Direction.SOUTH_EAST);
+		this.player.move(DELTA);
+		
+		int xmovement = (int) (Direction.SOUTH_EAST.getXRatio() * this.playerMovement);
+		int ymovement = (int) (Direction.SOUTH_EAST.getYRatio() * this.playerMovement);
+		
+		assertEquals(new Position(xmovement, ymovement), this.player.getPosition());
 	}
 }
