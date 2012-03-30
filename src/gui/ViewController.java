232876614f2b1e0d0d3ae20afbd7eba7e28d4ba7
Jesper Persson <JesperT.Persson@gmail.com>
2012-03-30 16:32:04 +0200

    GameModel now initates 5 zomibes for testing

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,9 +1,5 @@
 package gui;
 
-import graphics.bitmap.Bitmap;
-import graphics.bitmap.BitmapTool;
-import gui.tilemap.TileMap;
-
 import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
@@ -17,16 +13,16 @@ import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
-
+import model.Entity;
+import model.character.Player;
 import tools.GraphicalFPSMeter;
 import tools.Logger;
 import tools.TimerTool;
-
-
 import event.Event;
 import event.IEventHandler;
-
-import model.Entity;
+import graphics.bitmap.Bitmap;
+import graphics.bitmap.BitmapTool;
+import gui.tilemap.TileMap;
 
 
 /**
@@ -45,7 +41,7 @@ public class ViewController implements IEventHandler {
 	private TileMap gameMap;
 	private Bitmap screen;
 	private BufferedImage screenImage;
-		
+
 	private GraphicalFPSMeter fpsmeter;
 
 	public ViewController(KeyListener listener, int screenWidth, int screenHeight) {
@@ -53,11 +49,11 @@ public class ViewController implements IEventHandler {
 		SCREEN_HEIGHT = screenHeight;
 
 		screenImage = new BufferedImage(SCREEN_WIDTH, SCREEN_HEIGHT, BufferedImage.TYPE_INT_ARGB);
-		
+
 		screen = new Bitmap(SCREEN_WIDTH, SCREEN_HEIGHT,BitmapTool.getARGBarrayFromDataBuffer(screenImage.getRaster(), SCREEN_WIDTH, SCREEN_HEIGHT));
-		
+
 		this.fpsmeter = new GraphicalFPSMeter();
-		
+
 		initScene();
 
 		// Creating the frame
@@ -71,12 +67,12 @@ public class ViewController implements IEventHandler {
 		canvas.setFocusable(true);
 		canvas.requestFocusInWindow();
 		canvas.addKeyListener(listener);
-		
+
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 		frame.add(canvas);
 		frame.setVisible(true);
-		
+
 		// Setup the canvas and frame
 		canvas.createBufferStrategy(2);
 		this.bufferStrategy = canvas.getBufferStrategy();
@@ -88,38 +84,38 @@ public class ViewController implements IEventHandler {
 		this.player = new Actor(new Point(SCREEN_WIDTH / 2,
 				SCREEN_HEIGHT / 2));
 		try {
-			this.gameMap = new TileMap("rec/images/levels/l3.bmp");
+			this.gameMap = new TileMap("rec/images/levels/l2.bmp");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 
 	public void render(double dt) {
 		do {
 			do {
 				Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
-				
+
 				TimerTool.start("BlitVisible");
 				gameMap.blitVisibleTilesToBitmap(screen, 
 						new Rectangle(player.getPosition().x, player.getPosition().y, 
-						SCREEN_WIDTH, SCREEN_HEIGHT));
-				
+								SCREEN_WIDTH, SCREEN_HEIGHT));
+
 				TimerTool.stop();
-				
+
 				TimerTool.start("Player");
 				if (player.getCurrentFrame() != null) {
 					screen.blit(player.getCurrentFrame(), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 				} else 
 					Logger.log("Playerimage is null!");
 				TimerTool.stop();
-				
+
 				TimerTool.start("Draw image");
 				g.drawImage(screenImage, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, null);
 				TimerTool.stop();
-				
+
 				this.fpsmeter.tick(g);
-				
+
 				g.dispose();
 			} while (bufferStrategy.contentsRestored());
 			bufferStrategy.show();
@@ -129,9 +125,11 @@ public class ViewController implements IEventHandler {
 
 	@Override
 	public void onEvent(Event evt) {
-		Entity p = (Entity) evt.getValue();
-		player.setDirection(p.getDirection());
-		player.setPosition(p.getPosition());
-		Logger.log(evt.getProperty() + " " + p.getDirection());
+		if (evt.getValue() instanceof Player){
+			Entity p = (Entity) evt.getValue();
+			player.setDirection(p.getDirection());
+			player.setPosition(p.getPosition());
+			Logger.log(evt.getProperty() + " " + p.getDirection());
+		}
 	}
 }
--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -1,7 +1,12 @@
 package model;
 
-import model.character.*;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
 import model.character.Character;
+import model.character.Enemy;
+import model.character.Player;
 import constants.Direction;
 
 /**
@@ -11,22 +16,41 @@ import constants.Direction;
  *
  */
 public class GameModel implements IGameModel {
-	
+
 	private Character player;
-	
+	private ArrayList<Enemy> enemies;
+
 	public GameModel() {
 		this.player = new Player();
+		this.enemies = new ArrayList <Enemy>();
+		initEnemies();
 	}
-	
+
+	/**
+	 * Creates a number of zombies and adds them to the enemy-list
+	 */
+	private void initEnemies() {
+		for (int i = 0; i < 5; i++){
+			this.enemies.add(new Enemy(5,  30, new Rectangle (5,5),  new Dimension (5,5),  5,  5));
+			this.enemies.get(i).setDirection(Direction.WEST);
+		}
+
+
+	}
+
 	/**
 	 * Updates the game model.
 	 * @param dt The time since the last update.
 	 */
 	public void update(double dt) {
-
 		this.player.move(dt);
+		
+		for (int i = 0; i < this.enemies.size(); i++){
+			this.enemies.get(i).move(dt);
+			System.out.println("Enemy nr " + i + "has X-value: " + this.enemies.get(i).getPosition().x);
+		}
 	}
-	
+
 	/**
 	 * Updates the player's direction.
 	 * 
@@ -36,10 +60,10 @@ public class GameModel implements IGameModel {
 	public void updateDirection(Direction dir) {
 		this.player.setDirection(dir);
 	}
-	
+
 	public void stopPlayer(){
 		this.player.stop();
 	}
-	
-	
+
+
 }
