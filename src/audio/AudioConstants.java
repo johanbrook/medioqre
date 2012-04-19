28a24e4d5dbaf8cb9934b202b3c187d20ed0f6a4
Chris Nordqvist <chris.nordqvist@gmail.com>
2012-04-19 17:01:24 +0200

    Added methods to control FX volume

--- src/audio/AudioConstants.java
+++ src/audio/AudioConstants.java
@@ -5,14 +5,23 @@ public class AudioConstants {
 	public static final float zROLLOFF = 100;
 	
 	private float BGM_VOLUME = 1;
+	private float FX_VOLUME = 1;
 	
 	
 	public float getBGMVolume(){
 		return BGM_VOLUME;
 	}
 	
-	public void setBGMVolume(float f){
+	public void setVolume(float f){
 		BGM_VOLUME = f;
+	}
+	
+	public float getFXVolume(){
+		return FX_VOLUME;
+	}
+	
+	public void setFXVolume(float f){
+		FX_VOLUME = f;
 	}
 	
 
