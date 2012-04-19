f04c04ce0e0bc9f6d8493f073988b4da64c08e77
Chris Nordqvist <chris.nordqvist@gmail.com>
2012-04-19 16:59:24 +0200

    Added methods to Control BGM volume

--- src/audio/AudioConstants.java
+++ src/audio/AudioConstants.java
@@ -3,5 +3,17 @@ package audio;
 public class AudioConstants {
 
 	public static final float zROLLOFF = 100;
+	
+	private float BGM_VOLUME = 1;
+	
+	
+	public float getBGMVolume(){
+		return BGM_VOLUME;
+	}
+	
+	public void setBGMVolume(float f){
+		BGM_VOLUME = f;
+	}
+	
 
 }
