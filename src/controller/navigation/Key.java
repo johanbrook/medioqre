6d1a3a0b566f367a9050b70a23a19929ee2e36ff
Johan Brook <johan.jvb@gmail.com>
2012-04-24 10:49:15 +0200

    Got navigation and shooting working simultaneously
    - Refactored navigation system.
    - Frank can move in all eight directions.
    - Action keys, such as shoot and chose weapon (not implemented yet).

--- src/controller/navigation/Key.java
+++ src/controller/navigation/Key.java
@@ -6,24 +6,25 @@
 
 package controller.navigation;
 
+import model.character.Player;
+import constants.Direction;
+
 interface Callable {
 	public void action();
 }
 
 public class Key {
 	private String key;
-	private Callable callback;
+	protected Callable callback;
 	
 	public Key(String name, Callable callback) {
 		this.key = name;
-		this.callback = callback;
-		
-		System.out.println("*** NEW KEY ***");
+		this.callback = callback;		
 	}
 	
 	public void fire() {
-		System.out.println("***** FIRED ******");
-		callback.action();
+		if(callback != null)
+			callback.action();
 	}
 	
 	public String getKey() {
@@ -33,5 +34,45 @@ public class Key {
 	@Override
 	public String toString() {
 		return this.key;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) {
+			return true;
+		}
+		if(o == null || getClass() != o.getClass()){
+			return false;
+		}
+		
+		Key other = (Key) o;
+		return this.key.equals(other.key);
+	}
+}
+
+
+class NavigationKey extends Key {
+
+	private Direction direction;
+	
+	public NavigationKey(String name, Callable callback, Direction dir) {
+		super(name, callback);
+		this.direction = dir;
+	}
+	
+	
+	public void fire(Player target) {
+		super.fire();
+		target.start();
+		target.setDirection(this.direction);
+	}
+	
+	public Direction getDirection() {
+		return this.direction;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		return super.equals(o) && this.direction == ((NavigationKey) o).direction; 
 	}
 }
--- src/controller/navigation/NavigationController.java
+++ src/controller/navigation/NavigationController.java
@@ -2,8 +2,10 @@ package controller.navigation;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import model.character.Player;
@@ -16,8 +18,8 @@ import constants.Direction;
  */
 public class NavigationController implements KeyListener {
 	
-	private Set<Key> keys;
-	private Map<Integer, Direction> keyMap;
+	private Set<NavigationKey> navKeys;
+	private Map<Integer, Key> keyMap;
 	
 	private Player player;
 	
@@ -28,13 +30,12 @@ public class NavigationController implements KeyListener {
 	private final double EPSILON = 0.2;
 	
 	
-	private Key up;
-	private Key down;
-	private Key left;
-	private Key right;
+	private NavigationKey up;
+	private NavigationKey down;
+	private NavigationKey left;
+	private NavigationKey right;
 	private Key shoot;
 	
-	private Map<Integer, Key> keySet;
 	
 	/**
 	 * Create a new NavigationController, which controls a
@@ -43,182 +44,126 @@ public class NavigationController implements KeyListener {
 	 * @param game A game model
 	 */
 	public NavigationController(Player target) {
-		this.keys = new HashSet<Key>();
-		this.keyMap = new HashMap<Integer, Direction>();
-		this.cachedKeys = new HashSet<Integer>();
-		
 		this.player = target;
-		
-		this.keyMap.put(KeyEvent.VK_W, Direction.NORTH);
-		this.keyMap.put(KeyEvent.VK_A, Direction.WEST);
-		this.keyMap.put(KeyEvent.VK_S, Direction.SOUTH);
-		this.keyMap.put(KeyEvent.VK_D, Direction.EAST);
 		
 		initKeys();
 	}
 	
 	
 	private void initKeys() {
-		this.keySet = new HashMap<Integer, Key>();
+		this.navKeys = new HashSet<NavigationKey>();
+		this.cachedKeys = new HashSet<Integer>();
+		this.keyMap = new HashMap<Integer, Key>();
 		
-		this.up = new Key("up", new Callable() {
-			@Override
-			public void action() {
-				updatePlayerDirection(Direction.NORTH);
-			}
-			
-		});
-		
-		this.down = new Key("down", new Callable() {
-			@Override
-			public void action() {
-				updatePlayerDirection(Direction.SOUTH);
-			}
-			
-		});
-		
-		this.left = new Key("left", new Callable() {
-			@Override
-			public void action() {
-				updatePlayerDirection(Direction.WEST);
-			}
-			
-		});
-		
-		this.right = new Key("right", new Callable() {
-			@Override
-			public void action() {
-				updatePlayerDirection(Direction.EAST);
-			}
-			
-		});
+		this.up = new NavigationKey("up", null, Direction.NORTH);
+		this.down = new NavigationKey("down", null, Direction.SOUTH);
+		this.left = new NavigationKey("left", null, Direction.WEST);
+		this.right = new NavigationKey("right", null, Direction.EAST);
 		
 		this.shoot = new Key("shoot", new Callable() {
 			@Override
 			public void action() {
-				System.out.println("DID ATTACK");
+				System.out.println("** Attacked once **");
 				player.attack();
 			}
 		});
 		
-		this.keySet.put(KeyEvent.VK_W, this.up);
-		this.keySet.put(KeyEvent.VK_A, this.left);
-		this.keySet.put(KeyEvent.VK_S, this.down);
-		this.keySet.put(KeyEvent.VK_D, this.right);
-		this.keySet.put(KeyEvent.VK_SPACE, this.shoot);
+		this.keyMap.put(KeyEvent.VK_W, this.up);
+		this.keyMap.put(KeyEvent.VK_A, this.left);
+		this.keyMap.put(KeyEvent.VK_S, this.down);
+		this.keyMap.put(KeyEvent.VK_D, this.right);
+		this.keyMap.put(KeyEvent.VK_SPACE, this.shoot);
 	}
-	
-	
-	private void updatePlayerDirection(Direction dir) {
-		this.player.setDirection(dir);
-		this.player.start();
-	}
-	
-	private void stopPlayer() {
-		this.player.stop();
-	}
-	
-	
-	private void sendKeyAction(Key key, boolean keyIsPressed) {
-		String is = (keyIsPressed) ? "down" : "up";
-		System.out.println(key +" is "+is);
-		System.out.println("Keys are: "+this.keys);
-		
-//		if(this.isQuick){
-//			
-//			if(this.cachedKeys.contains(KeyEvent.VK_W) && this.cachedKeys.contains(KeyEvent.VK_A)) {
-//				updatePlayerDirection(Direction.NORTH_WEST);
-//			}
-//			else if(this.cachedKeys.contains(KeyEvent.VK_W) && this.cachedKeys.contains(KeyEvent.VK_D)) {
-//				updatePlayerDirection(Direction.NORTH_EAST);
-//			}
-//			else if(this.cachedKeys.contains(KeyEvent.VK_S) && this.cachedKeys.contains(KeyEvent.VK_A)) {
-//				updatePlayerDirection(Direction.SOUTH_WEST);
-//			}
-//			else if(this.cachedKeys.contains(KeyEvent.VK_S) && this.cachedKeys.contains(KeyEvent.VK_D)) {
-//				updatePlayerDirection(Direction.SOUTH_EAST);
-//			}
-//			
-//			this.isQuick = false;
-//			
-//			if(!keyIsPressed) {
-//				stopPlayer();
-//			}
-//			
-//			return;
-//		}
-//		
-		if(!keyIsPressed) {
-			stopPlayer();
-			return;
-		}
-		
-		
-//		if(this.keys.size() > 1){
-			if(this.keys.contains(down) && this.keys.contains(left)) {
-				updatePlayerDirection(Direction.SOUTH_WEST);
-			}
-			if(this.keys.contains(down) && this.keys.contains(right)) {
-				updatePlayerDirection(Direction.SOUTH_EAST);
-			}
-			if(this.keys.contains(up) && this.keys.contains(left)) {
-				updatePlayerDirection(Direction.NORTH_WEST);
-			}
-			if(this.keys.contains(up) && this.keys.contains(right)) {
-				updatePlayerDirection(Direction.NORTH_EAST);
-			}
-//		}
-//		
-		if(this.keys.size() == 1){
-			System.out.println("One key");
-			key.fire();
-		}
-		
-	}
-	
 
+	
+	
+	private void refreshDirection() {
+		if(this.navKeys.size() > 1){
+			createCompositeKey().fire(this.player);
+		}
+		else if(this.navKeys.size() == 1){
+			((NavigationKey)this.navKeys.toArray()[0]).fire(this.player);
+		}
+	}
+	
+	
+	private NavigationKey createCompositeKey() {
+
+		System.out.println("Creating composite: " + this.navKeys);
+		
+		if(this.navKeys.contains(up) && this.navKeys.contains(right)){
+			System.out.println("TOP RIGHT");
+			return new NavigationKey("top_right", null, Direction.NORTH_EAST);
+		}
+		
+		if(this.navKeys.contains(up) && this.navKeys.contains(left)){
+			System.out.println("TOP LEFT");
+			return new NavigationKey("top_left", null, Direction.NORTH_WEST);
+		}
+		
+		if(this.navKeys.contains(down) && this.navKeys.contains(right)){
+			System.out.println("BOTTOM RIGHT");
+			return new NavigationKey("bottom_right", null, Direction.SOUTH_EAST);
+		}
+		
+		if(this.navKeys.contains(down) && this.navKeys.contains(left)){
+			System.out.println("BOTTOM LEFT");
+			return new NavigationKey("bottom_left", null, Direction.SOUTH_WEST);
+		}
+		
+		
+		return null;
+		
+	}
+	
 	@Override
 	public void keyPressed(KeyEvent evt) {
+		Key a = this.keyMap.get(evt.getKeyCode());
 		
-		if(this.keySet.containsKey(evt.getKeyCode())) {
-			Key a = this.keySet.get(evt.getKeyCode());
-			this.keys.add(a);
+		if(a != null) {
 			
-			sendKeyAction(a, true);
-		}
-		
-		if(this.keyMap.containsKey(evt.getKeyCode())){
-			
-			this.cachedKeys.clear();
-//			sendKeyAction(evt, true);
+			if(a instanceof NavigationKey) {
+				this.navKeys.add((NavigationKey) a);
+				refreshDirection();
+			}
+			else{
+				a.fire();
+			}
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent evt) {
 		
-		if(this.keySet.containsKey(evt.getKeyCode())) {
-			Key a = this.keySet.get(evt.getKeyCode());
-			this.keys.remove(a);
-			sendKeyAction(a, !this.keys.isEmpty());
+		Key a = this.keyMap.get(evt.getKeyCode());
+		
+		if(a instanceof NavigationKey){
+			this.navKeys.remove(a);
+			refreshDirection();
 		}
 		
-		if(this.keyMap.containsKey(evt.getKeyCode())){
-			
-			if(this.keys.size() == 1 && this.cachedKeys.size() > 0) {
-				double seconds = ((System.nanoTime() - start) / 10E8);
-				this.isQuick = seconds < EPSILON;
-			}
-			else if(this.keys.size() == 2) {
-				this.start = System.nanoTime();
-			}
-			
-			
-			this.cachedKeys.add(evt.getKeyCode());
-			
+		
+		
+		if(this.navKeys.isEmpty()){
+			this.player.stop();
+		}
+		
+//		if(this.keyMap.containsKey(evt.getKeyCode())){
+//			
+//			if(this.keys.size() == 1 && this.cachedKeys.size() > 0) {
+//				double seconds = ((System.nanoTime() - start) / 10E8);
+//				this.isQuick = seconds < EPSILON;
+//			}
+//			else if(this.keys.size() == 2) {
+//				this.start = System.nanoTime();
+//			}
+//			
+//			
+//			this.cachedKeys.add(evt.getKeyCode());
+//			
 //			sendKeyAction(evt, !this.keys.isEmpty());
-		}
-		
+//		}
+//		
 	}
 
 	@Override
