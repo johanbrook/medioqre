ab7fd6ebb33930a81aefaec6c303ae586cf93609
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-04-29 18:59:45 +0200

    Implemented serialize, deserialize and the real rendering.

--- src/tilemap/Tile.java
+++ src/tilemap/Tile.java
@@ -1,11 +1,14 @@
 package tilemap;
 
+import java.util.Random;
+
 import graphics.opengl.Sprite;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import core.GLRenderableObject;
@@ -15,12 +18,20 @@ import datamanagement.SharedTextures;
 
 public class Tile implements JSONSerializable, GLRenderableObject{
 
-	private Rectangle rectangle;
+//	private Rectangle rectangle;
+	private Sprite sprite;
+	private int type;
 	
 	
-	public Tile()
+	// Testing shit
+	// ************************************************** WARNING **************************************************
+	// ********************************************* SHOULD BE REMOVED *********************************************
+	private Random random = new Random(); 
+	
+	public Tile(Sprite sprite, int type)
 	{
-		
+		this.type = type;
+		this.sprite = sprite;
 	}
 	
 	public Tile(JSONObject o)
@@ -28,32 +39,42 @@ public class Tile implements JSONSerializable, GLRenderableObject{
 		this.deserialize(o);
 	}
 	
+	// Getters 
+	public int getType()
+	{
+		return this.type;
+	}
+	
 	@Override
 	public void render(Rectangle object, Rectangle target, GLAutoDrawable canvas)
 	{
-		if (object != null && object.intersectsRectangle(target)) {
-
-			GL2 gl = canvas.getGL().getGL2();
-
-			float rX1 = (float) ((2.0f * object.getX()) - (float) target
-					.getWidth()) / (float) target.getWidth();
-			float rX2 = (float) (2.0f * (object.getX() + object.getWidth()) - (float) target
-					.getWidth()) / (float) target.getWidth();
-			float rY1 = (float) (2.0f * (object.getY() + object.getHeight()) - (float) target
-					.getHeight()) / (float) target.getHeight();
-			float rY2 = (float) (2.0f * object.getY() - (float) target
-					.getHeight()) / (float) target.getHeight();
-
-			gl.glBegin(GL2.GL_QUADS);
-			gl.glColor3f(1.0f, 0.0f, 1.0f);
-			
-			gl.glVertex2f(rX1, -rY2);
-			gl.glVertex2f(rX2, -rY2);
-			gl.glVertex2f(rX2, -rY1);
-			gl.glVertex2f(rX1, -rY1);
-
-			gl.glEnd();
-		}
+		
+		if (this.sprite != null)
+			this.sprite.render(object, target, canvas);
+		
+//		if (object != null && object.intersectsRectangle(target)) {
+//
+//			GL2 gl = canvas.getGL().getGL2();
+//
+//			float rX1 = (float) ((2.0f * object.getX()) - (float) target
+//					.getWidth()) / (float) target.getWidth();
+//			float rX2 = (float) (2.0f * (object.getX() + object.getWidth()) - (float) target
+//					.getWidth()) / (float) target.getWidth();
+//			float rY1 = (float) (2.0f * (object.getY() + object.getHeight()) - (float) target
+//					.getHeight()) / (float) target.getHeight();
+//			float rY2 = (float) (2.0f * object.getY() - (float) target
+//					.getHeight()) / (float) target.getHeight();
+//
+//			gl.glBegin(GL2.GL_QUADS);
+//			gl.glColor3f(random.nextFloat(), random.nextFloat(), random.nextFloat());
+//			
+//			gl.glVertex2f(rX1, -rY2);
+//			gl.glVertex2f(rX2, -rY2);
+//			gl.glVertex2f(rX2, -rY1);
+//			gl.glVertex2f(rX1, -rY1);
+//
+//			gl.glEnd();
+//		}
 	}
 
 	@Override
@@ -63,17 +84,34 @@ public class Tile implements JSONSerializable, GLRenderableObject{
 	@Override
 	public Rectangle getBounds()
 	{
-		return this.rectangle;
+//		return this.rectangle;
+		return this.sprite.getBounds();
 	}
 
 	@Override
 	public JSONObject serialize()
 	{
+		JSONObject retObj = new JSONObject();
+		try {
+			retObj.put("sprite", this.sprite.serialize());
+			retObj.put("type", this.type);
+			return retObj;
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public void deserialize(JSONObject o)
 	{
+		try {
+			this.sprite = new Sprite(o.getJSONObject("sprite"));
+			this.type = o.getInt("type");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
