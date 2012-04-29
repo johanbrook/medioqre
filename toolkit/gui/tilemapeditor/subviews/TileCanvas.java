65e1f8a6ea6a51e0347b31eaf6f743bf77a42496
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-04-29 16:31:30 +0200

    Added support to follow and render a certain tilemap.

--- toolkit/gui/tilemapeditor/subviews/TileCanvas.java
+++ toolkit/gui/tilemapeditor/subviews/TileCanvas.java
@@ -1,19 +1,23 @@
 package gui.tilemapeditor.subviews;
 
-import java.awt.Frame;
-
 import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLCapabilities;
 import javax.media.opengl.GLEventListener;
-import javax.media.opengl.GLProfile;
 import javax.media.opengl.awt.GLCanvas;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+
+import tilemap.TileMap;
 
 import com.jogamp.opengl.util.FPSAnimator;
 
+import core.Rectangle;
+
 public class TileCanvas extends GLCanvas implements GLEventListener {
+	
+	private TileMap currentTileMap;
+	
+	Rectangle object = new Rectangle( 0, 0, 100,100);
+	Rectangle target = new Rectangle( 0, 0, 100,100);
 	
 	public TileCanvas()
 	{
@@ -28,20 +32,36 @@ public class TileCanvas extends GLCanvas implements GLEventListener {
 		animator.start();
 	}
 
+	
+	public void setTileMap(TileMap tileMap)
+	{
+		this.currentTileMap = tileMap;
+	}
+	
 	@Override
 	public void display(GLAutoDrawable arg0)
 	{
 		GL2 gl = arg0.getGL().getGL2();
 		gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
-		gl.glBegin(GL2.GL_QUADS);
-		gl.glColor3f(1.0f, 0.0f, 0.0f);
 		
-		gl.glVertex2f(-0.5f, -0.5f);
-		gl.glVertex2f(-0.5f, 0.5f);
-		gl.glVertex2f(0.5f, 0.5f);
-		gl.glVertex2f(0.5f, -0.5f);
 		
-		gl.glEnd();
+		if (this.currentTileMap != null) {
+			System.out.println(object);
+			System.out.println(target);
+			this.currentTileMap.render(object, target, arg0);
+			
+		} else {
+			
+			gl.glBegin(GL2.GL_QUADS);
+			gl.glColor3f(1.0f, 0.0f, 0.0f);
+			
+			gl.glVertex2f(-0.5f, -0.5f);
+			gl.glVertex2f(-0.5f, 0.5f);
+			gl.glVertex2f(0.5f, 0.5f);
+			gl.glVertex2f(0.5f, -0.5f);
+			
+			gl.glEnd();
+		}
 	}
 
 	@Override
