3f33bdbce694ab72b7ab5789834d363877852c71
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-04-29 16:27:49 +0200

    Implemented super simple tile that just renders a purple square at its position.

--- src/tilemap/Tile.java
+++ src/tilemap/Tile.java
@@ -18,9 +18,9 @@ public class Tile implements JSONSerializable, GLRenderableObject{
 	private Rectangle rectangle;
 	
 	
-	public Tile(int x, int y, int width, int height)
+	public Tile()
 	{
-		this.rectangle = new Rectangle(x, y, width, height);
+		
 	}
 	
 	public Tile(JSONObject o)
@@ -31,7 +31,29 @@ public class Tile implements JSONSerializable, GLRenderableObject{
 	@Override
 	public void render(Rectangle object, Rectangle target, GLAutoDrawable canvas)
 	{
-		GL2 gl = canvas.getGL().getGL2();
+		if (object != null && object.intersectsRectangle(target)) {
+
+			GL2 gl = canvas.getGL().getGL2();
+
+			float rX1 = (float) ((2.0f * object.getX()) - (float) target
+					.getWidth()) / (float) target.getWidth();
+			float rX2 = (float) (2.0f * (object.getX() + object.getWidth()) - (float) target
+					.getWidth()) / (float) target.getWidth();
+			float rY1 = (float) (2.0f * (object.getY() + object.getHeight()) - (float) target
+					.getHeight()) / (float) target.getHeight();
+			float rY2 = (float) (2.0f * object.getY() - (float) target
+					.getHeight()) / (float) target.getHeight();
+
+			gl.glBegin(GL2.GL_QUADS);
+			gl.glColor3f(1.0f, 0.0f, 1.0f);
+			
+			gl.glVertex2f(rX1, -rY2);
+			gl.glVertex2f(rX2, -rY2);
+			gl.glVertex2f(rX2, -rY1);
+			gl.glVertex2f(rX1, -rY1);
+
+			gl.glEnd();
+		}
 	}
 
 	@Override
