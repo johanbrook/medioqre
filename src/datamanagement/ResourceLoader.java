ce59d0ae628023be41f4971edb8e56a3721dfed9
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-05-03 19:31:49 +0200

    Added functionality to load TileMaps and TileSheets from either absolute path or from resources.

--- src/datamanagement/ResourceLoader.java
+++ src/datamanagement/ResourceLoader.java
@@ -1,18 +1,27 @@
 package datamanagement;
 
+import graphics.tools.PixelCastingTool;
+
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.imageio.ImageIO;
+
 import org.apache.commons.io.IOUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import tilemap.TileMap;
+import tilemap.TileSheet;
 
 public class ResourceLoader {
 	
-	public static String loadStringFromAbsolutePath(String absolutePath)
+	public static String loadJSONStringFromAbsolutePath(String absolutePath)
 	{
-		InputStream inputStream;
 		try {
 			InputStream input = new FileInputStream(new File(absolutePath));
 			String load = IOUtils.toString(input);
@@ -26,10 +35,10 @@ public class ResourceLoader {
 		}
 		return null;
 	}
-	public static String loadStringFromResourceFolder(String source)
+	public static String loadJSONStringFromResources(String resource)
 	{
 		try {
-			InputStream input = ClassLoader.getSystemResourceAsStream("spritesheets/json/"+source);
+			InputStream input = ClassLoader.getSystemResourceAsStream("spritesheets/json/"+resource);
 			String load = IOUtils.toString(input);
 			return load;
 		} catch (IOException e) {
@@ -38,4 +47,79 @@ public class ResourceLoader {
 		}
 		return null;
 	}
+	public static TileMap loadTileMapFromAbsolutePath(String absolutePath)
+	{
+		try {
+			System.out.println(absolutePath);
+			BufferedImage img = ImageIO.read(new FileInputStream(new File(absolutePath)));
+			
+			int[] pixels = PixelCastingTool.getARGBarrayFromDataBuffer(img.getRaster(), img.getWidth(), img.getHeight());
+			
+			TileMap tileMap = new TileMap(img.getWidth(), img.getHeight(), null, pixels);
+			
+			return tileMap;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Could not load: " + absolutePath);
+		
+		return null;
+	}
+	public static TileMap loadTileMapFromResources(String resource)
+	{
+		try {			
+			BufferedImage img = ImageIO.read(ClassLoader.getSystemResource("spritesheets/levels/"+resource));
+			
+			int[] pixels = PixelCastingTool.getARGBarrayFromDataBuffer(img.getRaster(), img.getWidth(), img.getHeight());
+			
+			TileMap tileMap = new TileMap(img.getWidth(), img.getHeight(), null, pixels);
+			
+			return tileMap;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Could not load: " + resource);
+		
+		return null;
+	}
+	public static TileSheet loadTileSheetFromAbsolutePath(String absolutePath)
+	{
+		try {
+			InputStream input = new FileInputStream(new File(absolutePath));
+			
+			String load = IOUtils.toString(input);
+			return new TileSheet(new JSONObject(load));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public static TileSheet loadTileSheetFromResource(String resource)
+	{
+		try {
+			InputStream input = ClassLoader.getSystemResourceAsStream("spritesheets/json/"+resource);
+			String load = IOUtils.toString(input);
+			return new TileSheet(new JSONObject(load));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 }
