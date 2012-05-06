c45bf1694313e0789e67ba7d0b2fd51b6e5b6576
Johan Brook <johan.jvb@gmail.com>
2012-05-06 23:35:21 +0200

    Load Player from ObjectFactory

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -199,7 +199,7 @@ public class GameModel implements IGameModel, IMessageListener, IMessageSender {
 	}
 
 	private void initPlayer() {
-		this.player = new Player(30, new Rectangle(28, 48), new Dimension(20,20), 0, 16);
+		this.player = ObjectFactory.newPlayer();
 		this.player.setPosition(1000, 100);
 		this.objects.add(this.player);
 		this.player.addReceiver(this);
