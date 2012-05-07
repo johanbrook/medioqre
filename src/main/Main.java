15ad3e9ad33376b701240bcb096f151b4cb22184
Johan Brook <johan.jvb@gmail.com>
2012-05-07 18:36:28 +0200

    Version 0.5

--- src/main/Main.java
+++ src/main/Main.java
@@ -24,7 +24,7 @@ import static controller.AppController.DEBUG;
  */
 public class Main {
 	
-	public static final String VERSION = "0.2";
+	public static final String VERSION = "0.5";
 
 	/**
 	 * @param args
