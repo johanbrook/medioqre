4b569659d6e03a0a7d89f5f73a4eb6904a7c004e
Johan Brook <johan.jvb@gmail.com>
2012-05-09 23:10:20 +0200

    Generated Javadoc

---
+++ publish.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Push javadoc files to a website hosted by github <http://pages.github.com/>.
+# Before executing this script, generate the javadoc files into pcdoc
+#
+#David Fraser
+
+# find out the current branch so we know where to switch back
+OLD_BRANCH=`git branch --no-color | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+
+git checkout gh-pages || exit $?
+
+# Clear out the old files: (files which will be served)
+echo "Clean old docs ..."
+rm -rf docs/javadoc/* 
+
+# Replace them with new files and commit them:
+echo "Copying new files and comitting ..."
+cp -R -p docs/pcdoc docs/javadoc \
+&& git add docs/javadoc \
+&& git commit -a -m "Generated Javadoc"
+
+#Remove the generated doc
+echo "Clean generated docs ..."
+rm -rf docs/pcdoc/*
+
+echo "Pushing to GitHub gh-pages branch ..."
+#git push origin gh-pages || exit $?
+
+# Switch back to the old branch
+git checkout $OLD_BRANCH || exit $?
