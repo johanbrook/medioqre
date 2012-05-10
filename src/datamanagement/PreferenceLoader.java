a19574067cdb76497c1ca01c98c8c0a19ba30889
Chris Nordqvist <chris.nordqvist@gmail.com>
2012-05-10 20:20:39 +0200

    cleanup + javadoc

--- src/datamanagement/PreferenceLoader.java
+++ src/datamanagement/PreferenceLoader.java
@@ -10,32 +10,47 @@ import java.util.prefs.*;
  */
 
 public class PreferenceLoader {
-//	private static PreferenceLoader instance;
-	private static Preferences prefs = Preferences.userNodeForPackage(PreferenceLoader.class);;
 
-//	private PreferenceLoader() {
-//		prefs = Preferences.userNodeForPackage(PreferenceLoader.class);
-//	};
-//
-//	public static PreferenceLoader getInstance() {
-//		if (instance == null)
-//			instance = new PreferenceLoader();
-//
-//		return instance;
-//	}
+	private static Preferences prefs = Preferences.userNodeForPackage(PreferenceLoader.class);
 
+	/**
+	 * Gets float value
+	 * 
+	 * @param key options key
+	 * @param def default value
+	 * @return stored preference value for the key, if no value is found the default value provided is given.
+	 */
 	public static float getFloat(String key, float def) {
 		return prefs.getFloat(key, def);
 	}
 
+	/**
+	 * Stores float value in OS standard preference storage
+	 * 
+	 * @param key options key
+	 * @param value the value to store
+	 */
 	public static void putFloat(String key, float value) {
 		prefs.putFloat(key, value);
 	}
 	
+	/**
+	 * Gets boolean value
+	 * 
+	 * @param key options key
+	 * @param def default value
+	 * @return stored preference value for the key, if no value is found the default value provided is given.
+	 */
 	public static boolean getBoolean(String key, boolean def) {
 		return prefs.getBoolean(key, def);
 	}
 
+	/**
+	 * Stores boolean value in OS standard preference storage
+	 * 
+	 * @param key options key
+	 * @param value the value to store
+	 */
 	public static void putBoolean(String key, boolean value) {
 		prefs.putBoolean(key, value);
 	}
