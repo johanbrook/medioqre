4f435c5fa32e0a081dbefdd7ad6c9274f7b18678
John Barbero Unenge <john.barbero.unenge@gmail.com>
2012-05-18 11:05:14 +0200

    Have been working on animation logic.
    The sprites now returns coordinates for rendering.
    
    The animation has support to determin which sprit should be
    returned depending on how much time hase passed during the
    animation. It has support for both looping and non looping
    animations.
    
    Signed-off-by: John Barbero Unenge <john.barbero.unenge@gmail.com>

--- src/graphics/opengl/animation/Animation.java
+++ src/graphics/opengl/animation/Animation.java
@@ -1,61 +1,97 @@
 package graphics.opengl.animation;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import graphics.json.JSONSerializable;
 
-public class Animation implements JSONSerializable{
+public class Animation implements JSONSerializable {
 
 	private int animationTag;
+
 	private Sprite[] sprites;
-	
+
+	private boolean looping;
+
+	private int duration;
+
 	/**
-	 * Creates an Animation with no sprites and no animation tag.
+	 * Creates an Animation with no Sprites and no animation tag.
+	 * 
+	 * Default is to loop the animation when the Sprites are added.
 	 */
 	public Animation() {
-		
+		this(new Sprite[]{}, 0, true);
 	}
-	
+
+	/**
+	 * Creates an Animation with the given Sprites, animation duration and
+	 * whether to loop the animation or not.
+	 * 
+	 * @param sprites
+	 *            The Sprites
+	 * @param duration
+	 *            The duration of the animation
+	 * @param shouldLoop
+	 *            Whether to loop the animation or not
+	 */
+	public Animation(Sprite[] sprites, int duration, boolean shouldLoop) {
+		if (sprites == null)
+			throw new IllegalArgumentException();
+
+		this.sprites = sprites;
+		this.looping = shouldLoop;
+	}
+
 	/**
 	 * Creates an Animation and deserializes it from the given JSONObject.
 	 * 
-	 * @param o JSONObject to deserialize from
-	 * @throws JSONException If the JSONObject doesn't contain some of the required keys
+	 * @param o
+	 *            JSONObject to deserialize from
+	 * @throws JSONException
+	 *             If the JSONObject doesn't contain some of the required keys
 	 */
-	public Animation(JSONObject o) throws JSONException{
+	public Animation(JSONObject o) throws JSONException {
+		this();
 		this.deserialize(o);
 	}
-	
+
 	// Setters
 	/**
 	 * Set the animationTag.
 	 * 
-	 * @param animationTag The new animationTag
+	 * @param animationTag
+	 *            The new animationTag
 	 */
 	public void setAnimationTag(int animationTag) {
 		this.animationTag = animationTag;
 	}
-	
+
 	/**
-	 * Set the sprites of the Animation.
+	 * Set the sprites of the Animation. May be an empty array but not null.
 	 * 
-	 * @param sprites The new sprites
+	 * @param sprites
+	 *            The new sprites
+	 * @throws IllegalArgumentException
+	 *             If the given sprite array is null
 	 */
 	public void setSprites(Sprite[] sprites) {
+		if (sprites == null)
+			throw new IllegalArgumentException();
+
 		this.sprites = sprites;
 	}
-	
+
 	/**
 	 * Adds a sprite to the last position of the sprite array.
 	 * 
-	 * @param sprite The new Sprite
+	 * @param sprite
+	 *            The new Sprite
 	 */
-	public void addSprite(Sprite sprite)  {
+	public void addSprite(Sprite sprite) {
 
-		Sprite[] tempSprites = new Sprite[this.sprites == null
-				? 1
-				: this.sprites.length + 1];
+		Sprite[] tempSprites = new Sprite[this.sprites.length + 1];
 
 		for (int i = 0; i < tempSprites.length - 1; i++) {
 			tempSprites[i] = this.sprites[i];
@@ -64,35 +100,91 @@ public class Animation implements JSONSerializable{
 		tempSprites[tempSprites.length - 1] = sprite;
 		this.sprites = tempSprites;
 	}
-	
+
 	// Getters
 	/**
 	 * Get the animationTag.
+	 * 
 	 * @return The animationTag
 	 */
 	public int getAnimationTag() {
 		return this.animationTag;
 	}
-	
+
 	/**
 	 * Get the sprites.
+	 * 
 	 * @return The sprites
 	 */
 	public Sprite[] getSprites() {
 		return this.sprites;
 	}
-	
+
+	private long timeLastFrame;
+	private int timePassed;
+	/**
+	 * Returns the current sprite. May be null.
+	 * 
+	 * @return The current Sprite
+	 */
+	public Sprite getCurrentSprite() {
+		long timeThisFrame = System.nanoTime() / 1000000;
+		int dt = (int) (timeThisFrame - this.timeLastFrame);
+		this.timePassed += dt;
+		this.timeLastFrame = timeThisFrame;
+
+		if (this.timePassed < this.duration) {
+			if (this.sprites != null) return null;
+			
+			return this.sprites[(int) ((int) ((float) this.timePassed / (float) duration) * (float) (this.sprites.length - 1))];
+		} else {
+			if (this.looping) {
+				this.startAnimation();
+				return this.getCurrentSprite();
+			}
+			return (this.sprites == null ? null : this.sprites[0]);
+		}
+	}
+
+	/**
+	 * Starts the animation.
+	 */
+	public void startAnimation() {
+		this.timeLastFrame = System.nanoTime() / 1000000;
+		this.timePassed = 0;
+	}
+
 	@Override
 	public JSONObject serialize() throws JSONException {
 		JSONObject retObj = new JSONObject();
+
+		JSONArray jsonArray = new JSONArray();
+		for (int i = 0; i < (this.sprites == null ? 0 : this.sprites.length); i++) {
+			if (this.sprites[i] == null) continue;
+			
+			jsonArray.put(i, this.sprites[i].serialize());
+		}
 		
 		retObj.put("animationTag", this.animationTag);
+		retObj.put("sprites", jsonArray);
+		retObj.put("looping", this.looping);
+		retObj.put("duration", this.duration);
 		return retObj;
 	}
 
 	@Override
-	public void deserialize(JSONObject o) throws JSONException{
+	public void deserialize(JSONObject o) throws JSONException {
 		this.animationTag = o.getInt("animationTag");
+		
+		JSONArray jsonArray = o.getJSONArray("sprites");
+		this.sprites = new Sprite[jsonArray.length()];
+		for (int i = 0; i < jsonArray.length(); i++) {
+			this.sprites[i] = new Sprite(jsonArray.getJSONObject(i));
+		}
+		
+		this.looping = o.getBoolean("looping");
+		this.duration = o.getInt("duration");
+		
 	}
 
 }
--- src/graphics/opengl/animation/Sprite.java
+++ src/graphics/opengl/animation/Sprite.java
@@ -5,16 +5,20 @@ import org.json.JSONObject;
 
 import graphics.json.JSONSerializable;
 import graphics.opengl.core.Rectangle;
+import graphics.opengl.core.Size;
 
 public class Sprite implements JSONSerializable {
 
-	private Rectangle rect;
+	private Rectangle textureRectangle;
+	private Size offset;
 
 	// Constructors
 	/**
 	 * Creates an empty Sprite with no variables set.
 	 */
 	public Sprite() {
+		this.textureRectangle = new Rectangle(0, 0, 0, 0);
+		this.offset = new Size(0, 0);
 	}
 
 	/**
@@ -23,6 +27,7 @@ public class Sprite implements JSONSerializable {
 	 * @throws JSONException If the JSONObject doesn't contain some of the required keys 
 	 */
 	public Sprite(JSONObject o) throws JSONException {
+		this();
 		this.deserialize(o);
 	}
 
@@ -33,7 +38,7 @@ public class Sprite implements JSONSerializable {
 	 * @param rectangle The rectangle
 	 */
 	public void setRectangle(Rectangle rectangle) {
-		this.rect = rectangle;
+		this.textureRectangle = rectangle;
 	}
 
 	// Getters
@@ -43,19 +48,49 @@ public class Sprite implements JSONSerializable {
 	 * @return The rectangle
 	 */
 	public Rectangle getRectangle() {
-		return this.rect;
+		return this.textureRectangle;
 	}
+	
+	// Methods
+	/**
+	 * Gets the points with instructions regarding how to render the sprite. 
+	 * 
+	 * The first two values are the texture coordinates, the two following 
+	 * are the vertex coordinates and the last two are the texture offset.
+	 * 
+	 * The texture and offset measurements are in PIXELS, and thus have to 
+	 * be scaled into floats between 0 and 1.
+	 * 
+	 * The vertex coordinates are either 0 or 1 and should be scaled as needed.
+	 * 
+	 * @return
+	 */
+	public float[] getVertexPoints() {
+		float[] points = new float[4];
+		
+		points[0] = this.textureRectangle.getX();
+		points[1] = this.textureRectangle.getY();
+		points[2] = this.textureRectangle.getWidth();
+		points[3] = this.textureRectangle.getHeight();
+		points[4] = this.offset.getWidth();
+		points[5] = this.offset.getHeight();
+		
+		return points;
+	}
+	
 	// Interface
 	@Override
 	public JSONObject serialize() throws JSONException {
 		JSONObject retObj = new JSONObject();
 
-		if (this.rect != null) {
+		if (this.textureRectangle != null) {
 			JSONObject jsonRect = new JSONObject();
-			jsonRect.put("x", this.rect.getX());
-			jsonRect.put("y", this.rect.getY());
-			jsonRect.put("width", this.rect.getWidth());
-			jsonRect.put("height", this.rect.getHeight());
+			jsonRect.put("x", this.textureRectangle.getX());
+			jsonRect.put("y", this.textureRectangle.getY());
+			jsonRect.put("width", this.textureRectangle.getWidth());
+			jsonRect.put("height", this.textureRectangle.getHeight());
+			jsonRect.put("offsX", this.offset.getWidth());
+			jsonRect.put("offsY", this.offset.getHeight());
 
 			retObj.put("rect", jsonRect);
 		}
@@ -66,10 +101,13 @@ public class Sprite implements JSONSerializable {
 	public void deserialize(JSONObject o) throws JSONException {
 		JSONObject jsonRect = o.getJSONObject("rect");
 		int x = jsonRect.getInt("x");
-		int y = jsonRect.getInt("x");
-		int width = jsonRect.getInt("x");
-		int height = jsonRect.getInt("x");
-		this.rect = new Rectangle(x, y, width, height);
+		int y = jsonRect.getInt("y");
+		int width = jsonRect.getInt("width");
+		int height = jsonRect.getInt("height");
+		int offsX = jsonRect.getInt("offsX");
+		int offsY = jsonRect.getInt("offsY");
+		this.textureRectangle = new Rectangle(x, y, width, height);
+		this.offset = new Size(offsX, offsY);
 	}
 
 }
